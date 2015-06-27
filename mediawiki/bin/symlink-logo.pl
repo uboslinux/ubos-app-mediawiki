24d341f5dd043255a01dc5c29286a028884fd6c9
Johannes Ernst <jernst@indiecomputing.com>
2015-06-27 01:44:39 +0000

    track ubos-admin

--- mediawiki/bin/symlink-logo.pl
+++ mediawiki/bin/symlink-logo.pl
@@ -13,14 +13,14 @@ my $logoFile  = $config->getResolveOrNull( 'installable.customizationpoints.wiki
 my $dir       = $config->getResolveOrNull( 'appconfig.apache2.dir' );
 my $localLogo = "$dir/_/ubos-images/logo.png";
 
-if( 'install' eq $operation ) {
+if( 'deploy' eq $operation ) {
     if( $logoFile && -f $logoFile ) {
         unless( UBOS::Utils::symlink( $logoFile, $localLogo )) {
             error( 'Symlink could not be created:', $localLogo );
         }
     }
 }
-if( 'uninstall' eq $operation ) {
+if( 'undeploy' eq $operation ) {
     if( -l $localLogo || -e $localLogo ) {
         unless( UBOS::Utils::deleteFile( $localLogo )) {
             error( 'Symlink could not be deleted:', $localLogo );
